af15d00aa9b33127269e73ca355bf4ddc2e66cdb
maksim <maksim.buyanov@gmail.com>
2023-11-05 22:03:44 +0300

    add queryId from tg

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-console.log(window.Telegram.WebApp)
+console.log(window.Telegram.WebApp);
 
-const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp;
 
-tg.MainButton.show()
-const submitButton = document.querySelector('#submit')
-const form = document.querySelector('form')
+tg.MainButton.show();
+const submitButton = document.querySelector("#submit");
+const form = document.querySelector("form");
 
-form.addEventListener('submit',(e)=> {
-  e.preventDefault()
-  const login = form.elements.login.value
-  const password = form.elements.password.value
-  debugger
-})
-
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-  const login = form.elements.login.value
-  const password = form.elements.password.value
-  const data = JSON.stringify({login,password})
-  tg.sendData(data);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  debugger;
 });
 
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  const queryId = tg.initDataUnsafe?.query_id;
+  const data = JSON.stringify({ login, password, queryId });
+  tg.sendData(data);
+});
